ffc45912abea702b1a00f54f6ae721f947590228
sondre haram <sondreharam@gmail.com>
2023-01-20 19:45:03 +0100

    regner ut arealet av en sirkel basert på input av radius i cm

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+#include <cmath>
+
+using namespace std;
 
 int main() {
-    double x = 10;
-    int y = 5;
-    double z = (x + 10)/(3*y);
-    std::cout << z;
-    return 0;
+    cout << "Enter radius in cm: ";
+    double radius;
+    cin >> radius;
+    const double pi = 3.14;
 
-
-
+    double areal = pi * pow(radius,2);
+    cout << "Arealet av sirkelen er:" << areal << "cm^2";
 
     return 0;
 }
